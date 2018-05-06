8a61aedf617f24e90de6cd2c99dc91fa25c51dfd
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-05 22:36:38 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('pickle burger');
+INSERT INTO burgers (burger_name) VALUES ('blackbean burger');
+INSERT INTO burgers (burger_name) VALUES ('classic burger');
+INSERT INTO burgers (burger_name) VALUES ('bison burger');
+INSERT INTO burgers (burger_name) VALUES ('pizza burger');
