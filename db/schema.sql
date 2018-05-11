66098ee6bca4fc1e2bc4021c987a60b99531bee2
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-11 00:30:30 -0600

    i'm committed to this

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+	burgers_name varchar(255) NOT NULL,
 	ordered BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
 
-INSERT INTO burgers (name) VALUES ('cheese burger');
-INSERT INTO burgers (name) VALUES ('pickle burger');
-INSERT INTO burgers (name) VALUES ('blackbean burger');
-INSERT INTO burgers (name) VALUES ('classic burger');
-INSERT INTO burgers (name) VALUES ('bison burger');
-INSERT INTO burgers (name) VALUES ('pizza burger');
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('pickle burger');
+INSERT INTO burgers (burger_name) VALUES ('blackbean burger');
+INSERT INTO burgers (burger_name) VALUES ('classic burger');
+INSERT INTO burgers (burger_name) VALUES ('bison burger');
+INSERT INTO burgers (burger_name) VALUES ('pizza burger');
