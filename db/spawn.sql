f080bb97b1476cff1680482c1f0f1a3ff25ffe64
Koltyn Palmer <koltynchiaro@gmail.com>
2018-05-12 13:22:08 -0600

    heroku app down fix attempt 1

--- db/spawn.sql
+++ db/spawn.sql
@@ -1,7 +1,7 @@
 USE burgers_db;
 ALTER TABLE burgers;
 -- ADD COLUMN nonburger_name BOOLEAN AFTER burger_name, 
-ADD COLUMN b_ordered BOOLEAN AFTER ordered, 
+ADD COLUMN b_ordered BOOLEAN,
 -- ADD COLUMN gorgonzola BOOLEAN AFTER ordered, 
 -- ADD COLUMN topping2 BOOLEAN AFTER ordered,
 -- ADD COLUMN topping3 BOOLEAN AFTER ordered,
@@ -9,4 +9,4 @@ ADD COLUMN b_ordered BOOLEAN AFTER ordered,
 -- ADD COLUMN topping5 BOOLEAN AFTER ordered,
 -- ADD COLUMN topping6 BOOLEAN AFTER ordered
 
---  mysql --host=127.0.0.1 --port=3306 --user=root --password=root
+--  mysql --host=127.0.0.1 --port=3306 --user=root --password=rootexit
